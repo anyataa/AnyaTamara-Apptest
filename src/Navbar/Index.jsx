cd7c768f9431b921648fb3096f4296acce80ccd7
Anya Tamara <anyatmr@gmail.com>
2021-09-02 11:30:49 +0700

    add notification

--- src/Navbar/Index.jsx
+++ src/Navbar/Index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { FaBeer, FaStar } from "react-icons/fa";
+import { BsFillChatSquareDotsFill } from "react-icons/bs";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
       <div className="container">
         <div className="d-flex justify-content-between nav-container">
           <h2 className="label-brand">Contactify</h2>
@@ -11,36 +12,43 @@ export const Navbar = () => {
             <div className="profile-img">
               <input id="notif-btn" type="checkbox" hidden={true} />
               <label htmlFor="notif-btn">
-                <h2 className="fa fa-bell icon-notif">
-                  <FaStar className="label-brand" />
+                <h2 className="fa fa-bell icon-notif label-brand">
+                  click to know more &nbsp;&nbsp;&nbsp;&nbsp;
+                  <BsFillChatSquareDotsFill className="label-brand" />
                 </h2>
               </label>
               {/* <!-- Nav Bar Body --> */}
               <div className="wrapper-notification">
-                <p className="detail-notif-duration">Today</p>
+                <p className="detail-notif-duration">Did you know?</p>
                 <li>
                   <div className="card-notification">
-                    <div className="card-notification" style={{ height: 195 }}>
-                      <p className="col-grey">
-                        Oopsie, You Have No Transaction History Yet For Today. .
-                        .{" "}
-                      </p>
-
-                      <img />
-                    </div>
-
-                    <p className="detail-notif-duration">This Week</p>
+                    <p className="detail-notif-duration">
+                      Based on NBC{" "}
+                      <span
+                        className="col-secondary"
+                        style={{ fontWeight: "bold" }}
+                      >
+                        75% people
+                      </span>{" "}
+                      feeling disconnected
+                    </p>
 
                     <div className="notif-detail">
                       <p></p>
-                      <h2></h2>
                     </div>
                   </div>
                 </li>
 
-                <div className="card-notification" style={{ height: 195 }}>
+                <div className="card-notification" style={{ height: "250px" }}>
                   <p className="col-grey">
-                    Oopsie, You Have No Transaction History Yet For This Week. .
+                    <span
+                      className="col-secondary"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Contactify
+                    </span>{" "}
+                    was invented in order to help and remind you to get
+                    connected again with everyone around you. See you soon.
                   </p>
                   <img />
                 </div>

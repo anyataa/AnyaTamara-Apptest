691dc956af808dfdf0ccf40f5b98a210862093ad
Anya Tamara <anyatmr@gmail.com>
2021-09-02 21:44:55 +0700

    styling the footer css component to become prettier

--- src/Footer/Index.js
+++ src/Footer/Index.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div className="bottom-bar  bg-green ">
       <div className="bottom-bar-container">
-        <p className="">2021 Anya-Contactify</p>
+        <p>2021 Anya-Contactify</p>
         <p>
           Contact Me : +62 8xxxxxxxxx
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
